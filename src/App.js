e3137d79f3d496775c8959952f510b3fa62da34e
PreethiPantangi <pantangisaipreethi@gmail.com>
2021-04-22 22:18:00 +0530

    added routing links for pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import HomePageComponent from './HomePage/HomePage'
+import BookDetailsComponent from './BookDetails/BookDetails'
 
 function App() {
   return (
     <div className="App">
-
+      <HashRouter>
+        <Switch>
+          <Route exact path='/' component={HomePageComponent} />
+          <Route exact path='category/:name' component={BookDetailsComponent} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
